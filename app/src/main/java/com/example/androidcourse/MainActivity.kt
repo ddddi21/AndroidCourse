0c1d1c130aa2f790cec856de2c74b02942efeaf1
ddddi21 <dilyra.as@gmail.com>
2020-09-21 02:27:47 +0300

    add hw-2

--- app/src/main/java/com/example/androidcourse/MainActivity.kt
+++ app/src/main/java/com/example/androidcourse/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.androidcourse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv_phone.setOnClickListener{
+            tv_phone.visibility = View.INVISIBLE
+            edit_phone.isCursorVisible = true
+            edit_phone.visibility = View.VISIBLE
+            tv_change_phone.visibility = View.INVISIBLE
+            btn_edit_is_done.visibility = View.VISIBLE
+        }
+        btn_edit_is_done.setOnClickListener{
+            edit_phone.visibility = View.INVISIBLE
+            btn_edit_is_done.visibility = View.INVISIBLE
+            tv_phone.visibility = View.VISIBLE
+            tv_phone.text = edit_phone.text
+            tv_change_phone.visibility = View.VISIBLE
+        }
+
     }
+
 }
